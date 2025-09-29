832746187f64dafda526f9825397e8633b3b5704
Theophile <theophile.benzaria@gmail.com>
2025-09-29 13:56:36 +0200

    Structure de dossier

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-if __name__ == '__main__':
-    print('Hello World')
 
 
 
 def multiply(a : int, b: int) -> int :
     return a * b
+
+
+if __name__ == '__main__':
+    print(multiply(2, 3))
+    print(multiply(2.4, 3))
+    print(multiply(2, 3))
+    print('Hello World')

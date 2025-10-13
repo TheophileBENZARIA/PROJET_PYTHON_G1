a815cfcc10c0251a1de944f552e625a26ceac75c
Theophile <theophile.benzaria@gmail.com>
2025-10-13 10:24:53 +0200

    Creation des fichiers map et gestion de la direction dans la génération de la map visuelle

--- Affichage/Terminal/AfficheTerm.py
+++ Affichage/Terminal/AfficheTerm.py
@@ -1,69 +1,110 @@
-
 import argparse
-
 import curses
-import os
+
+"""
+In : [f: file] <- fichier ouvert en lecture
+Out : [grille: list] <- tableau 2D
+A partir du fichier map elle genère la grille d'affichage et en duplicant en mirroir les troupes
+"""
+def genererGrille(f) -> list:
+    grille = []
+    # Recupèrer la longeur, la hauteur et l'orientation a la première ligne du fichier
+    read = f.readline()
+    dims = read.replace(" ", "").replace("\n", "").split(",")
+    hauteur, longeur, orientation = int(dims[0]), int(dims[1]), dims[2]
+    assert hauteur > 0
+    assert longeur > 0
+    assert orientation == "G" or orientation == "D" or orientation == "H" or orientation == "B"
+
+    for i in range(hauteur):
+        line = " "
+        try: line = f.readline().replace(".", " ").replace("\n", " ")
+        except: pass
+
+        grille.append(list(line))
+        while len(grille[i]) > longeur:
+            del grille[i][-1]
+        while len(grille[i]) < longeur:
+            grille[i].append(' ')
+
+        if orientation == "D": grille[i] = grille[i] + grille[i][::-1]
+        if orientation == "G": grille[i] = grille[i][::-1] + grille[i]
+
+    if orientation == "B": grille = grille + grille[::-1]
+    if orientation == "H": grille = grille[::-1] + grille
+    return grille
+
+
+
+
+
 
 def main(stdscr):
-    curses.curs_set(0)            # hide cursor
-    stdscr.nodelay(True)         # non-blocking getch
-    stdscr.keypad(True) # enable special keys capture
+    curses.curs_set(0)  # hide cursor
+    stdscr.nodelay(True)  # non-blocking getch
+    stdscr.keypad(True)  # enable special keys capture
     max_hauteur, max_longeur = stdscr.getmaxyx()
 
     # recuperation de l'argument map
     parser = argparse.ArgumentParser(description="Argument pour lancer l'affichage")
-    parser.add_argument("--mapFilePath", type=str, required=True, help="Fichier de carte à charger")
+    parser.add_argument("--save", type=str, required=True, help="Fichier de sauvegarde à charger")
+    parser.add_argument("--map", type=str, required=True, help="Fichier de carte à charger lié à la sauvegarde")
     args = parser.parse_args()
 
-    #assert os.path.exists(args.mapFilePath)  #test si le ficher existe
+    # ! Tester si la map est bien la meme que celle noter dans la save
 
-    grille = []
+    grille = [[]]
 
-    with open(args.mapFilePath, "r") as f:
-        #Recupèrer la longeur, la hauteur et l'orientation a la première ligne du fichier
-        read = f.readline()
-        dims = read.replace(" ", "").split(",")
-        hauteur, longeur, orientation = int(dims[0]), int(dims[1]), str(dims[2])
-        assert hauteur > 0 and hauteur <= max_hauteur//2-1
-        assert longeur > 0 and longeur <= max_longeur//2-1
-
-        print(orientation, orientation=="V",ord("V"))
-        #assert orientation == "V" or orientation == "H"
-
-        for i in range(hauteur):
-            line = " "
-            try : line = f.readline().replace(".", " ").replace("\n", " ")
-            except : pass
-            print(list(line))
-            grille.append(list(line))
-            while len(grille[i]) < longeur:
-                grille[i].append(' ')
-            if orientation == "V": grille[i] += grille[i][::-1]
-        if orientation == "H": grille += grille[::-1]
+    with open(args.map, "r") as f:
+        grille = genererGrille(f)
 
 
-    stdscr.addstr(0, 0, "_" * (longeur + 2))
+    stdscr.addstr(0, 0, "_" * (len(grille[0]) + 2))
 
-    for i in range(hauteur):
+    for i in range(len(grille)):
         stdscr.addstr(i + 1, 0, "|")
-        stdscr.addstr(i + 1, 1, "".join(grille[i])+"|")
+        stdscr.addstr(i + 1, 1, "".join(grille[i]) + "|")
 
-    stdscr.addstr(hauteur, 0, "_" * (longeur + 2))
+    stdscr.addstr(len(grille)+1, 0, "‾" * (len(grille[0]) + 2))
 
-    while True :
+
+
+
+    #-------------------------------------------------------------------------------------
+    curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
+    while True:
         key = stdscr.getch()
 
+        if key == ord('q'):
+            break
 
+        # Si c’est un événement souris
+        try:
+            _, x, y, _, bstate = curses.getmouse()
 
+            # Efface la ligne du bas
+            stdscr.move(curses.LINES - 1, 0)
+            stdscr.clrtoeol()
 
-
-        if key == -1:
-            # aucune touche pressée
+            # Affiche les infos de la souris
+            stdscr.addstr(
+                curses.LINES - 1, 0,
+                f"Souris à (x={x}, y={y}) | Boutons: {bstate}"
+            )
+            stdscr.refresh()
+        except curses.error:
+            # Erreur possible si les coords sont hors écran
             pass
-        else :
 
-            stdscr.addstr(1, 0, chr(key))
-        curses.napms(20)
+    if key == -1:
+        # aucune touche pressée
+        pass
+    else:
+
+        stdscr.addstr(1, 0, chr(key))
+    curses.napms(20)
+    #-------------------------------------------------------------------------------------
+
 
 if __name__ == "__main__":
     curses.wrapper(main)

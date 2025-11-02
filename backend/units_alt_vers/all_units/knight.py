ce72acaa0097a6077dd59d267cc2c33e10841a51
mehdi15649 <mehdi.stpaul@gmail.com>
2025-11-02 18:36:30 +0100

    Update knight.py

--- backend/units_alt_vers/all_units/knight.py
+++ backend/units_alt_vers/all_units/knight.py
@@ -1,1 +1,16 @@
+from .unit import BaseUnit2
+
+class Knight(BaseUnit):
+    def __init__(self):
+        super().__init__(
+            name="Knight",
+            hp=100,
+            attack=10,
+            armor=2,
+            pierce_armor=2,
+            attack_range=0,
+            speed=1.5,
+            reload_time=1.8,
+            classes=["Cavalry"]
+        )
 

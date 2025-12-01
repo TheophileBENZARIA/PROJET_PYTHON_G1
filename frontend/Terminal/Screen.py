5ec048b7f510152be3a6ad4fc684c333915d5c5a
Théophile BENZARIA <theophile.benzaria@gmail.com>
2025-12-01 15:52:29 +0100

    refondement d'afficher grille

--- frontend/Terminal/Screen.py
+++ frontend/Terminal/Screen.py
@@ -16,41 +16,82 @@ class Screen :
 
     def getch(self):
         key =self.std.getch()
-        self.std.addstr(0,0, f"{key}")
+        if key == 259 and self.y >0:# up arrow
+            self.y-=1
+        if key == 258 : #down arrow
+            self.y+=1
+        if key == 260 and self.x >0: #right arrow
+            self.x-=1
+        if key == 261 : #left arrow
+            self.x+=1
 
 
     def afficher_grille(self, grille):
-        ly = len(grille)
-        if (ly > 0) :
-            lx = len(grille[0])
-            max_y, max_x = self.std.getmaxyx()
-            max_y-=2
+        x_n = 0
+        y_n = 0
 
-            #right border
-            c = "|"
-            if (self.x > 0) :
+        maxy, maxx = self.std.getmaxyx()
+        maxy-=3
+        maxx-=2
+
+        if abs(maxy - len(grille)) < self.y :
+            self.y = abs(maxy - len(grille))
+        min_y = self.y
+        max_y = maxy + self.y
+        if  maxy > len(grille) :
+            y_n = 1
+            min_y = 0
+            max_y = len(grille)
+        elif maxy == len(grille) - self.y:
+            min_y = self.y
+            max_y = maxy + self.y
+            y_n =2
+        elif self.y == 0:
+                y_n = 4
+        else :
+            y_n=3
+
+        if abs(maxx - len(grille)) < self.x :
+            self.x = abs(maxx - len(grille))
+        min_x = self.x
+        max_x = maxx + self.x
+        if len(grille) == 0 :
+            x_n = 1
+            min_x = 0
+            max_x = 0
+        elif maxx > len(grille[0]):
+            x_n = 1
+            min_x = 0
+            max_x = len(grille[0])
+        elif maxx == len(grille[0]) - self.x:
+            min_x = self.x
+            max_x = maxx + self.x
+            x_n = 2
+        elif self.x == 0 :
+            x_n = 4
+        else:
+            x_n = 3
+
+        for y in range(min_y, max_y):
+            c= "|"
+            if x_n == 2 or x_n == 3 :
                 c="."
-            for i in range(1,min(max_y-2, ly)+1) :
-                self.std.addstr(i,0,c)
+            self.std.addstr(1+y-min_y,0,c)
 
-            for i in range(1,min(lx-self.x, max_x-2)+1) :
-
-                c="_"
-                if self.y >0:
-                    c="."
-                self.std.addstr(0,i,c)
-
-                for j in range(1,min(ly-self.y,max_y-2)+1) :
-                    self.std.addstr(j,i,str(grille[j-1][i-1]))
-
+        for x in range(min_x,max_x) :
+            c = "_"
+            if y_n == 2 or y_n == 3:
                 c = "."
-                if min(ly-self.y,max_y-2) == ly-self.y:
-                    c = "‾"
-                self.std.addstr(min(ly-self.y,max_y-2)+1, i, c)
+            self.std.addstr(0,1+x-min_x,c)
+            for y in range(min_y, max_y) :
+                self.std.addstr(1 + y - min_y, 1+x-min_x, str(grille[y][x]))
+            c = "‾"
+            if y_n == 3 or y_n == 4:
+                c = "."
+            self.std.addstr(1 + max_y - min_y, 1+x-min_x,c)
 
-            #left border
-            c = "."
-            if  min(lx-self.x,max_x-2) == lx-self.x:
-                c = "|"
-            for i in range(1, min(max_y - 2, ly) + 1):
-                self.std.addstr(i, min(lx-self.x,max_x-2)+1, c)
+        for y in range(min_y, max_y):
+            c= "|"
+            if x_n == 4 or x_n == 3 :
+                c="."
+            self.std.addstr(1+y-min_y,max_x+1,c)

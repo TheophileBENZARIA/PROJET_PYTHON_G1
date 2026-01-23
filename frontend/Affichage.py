a2c4759152ffd0bcca5c8543834eb0e4a92f7b24
Malek <malek.abb55@gmail.com>
2026-01-23 16:57:04 +0100

    main fix

--- frontend/Affichage.py
+++ frontend/Affichage.py
@@ -20,7 +20,7 @@ class Affichage(ABC) :
     def get_sizeMap(cls, map, army1, army2):
         x_max , y_max, x_min, y_min = 0,0,0,0
 
-        for unit in army2.livings_unit()+ army1.livings_unit() :
+        for unit in army2.living_units()+ army1.living_units() :
             if unit.position[0] > x_max:
                 x_max =unit.position[0]
             if unit.position[1] > y_max:

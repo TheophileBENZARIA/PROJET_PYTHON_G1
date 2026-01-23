176828517e2c00fa67bf8d87cef3654c26deb3d4
Malek <malek.abb55@gmail.com>
2026-01-23 20:22:52 +0100

    .

--- frontend/Affichage.py
+++ frontend/Affichage.py
@@ -13,41 +13,41 @@ class Affichage(ABC) :
         pass
 
     @abstractmethod
-    def afficher(self, game_map: Map, army1: Army, army2: Army):
+    def afficher(self,map:Map, army1:Army, army2:Army):
         pass
 
     @classmethod
-    def get_sizeMap(cls, game_map, army1, army2):
+    def get_sizeMap(cls, map, army1, army2):
         x_max , y_max, x_min, y_min = 0,0,0,0
 
-        for unit in army2.living_units():
+        for unit in army2.living_units() :
             if unit.position[0] > x_max:
-                x_max = unit.position[0]
+                x_max =unit.position[0]
             if unit.position[1] > y_max:
-                y_max = unit.position[1]
+                y_max =unit.position[1]
             if unit.position[0] < x_min:
-                x_min = unit.position[0]
+                x_min =unit.position[0]
             if unit.position[1] < y_min:
-                y_min = unit.position[1]
+                y_min =unit.position[1]
 
-        for unit in army1.living_units():
+        for unit in army1.living_units() :
             if unit.position[0] > x_max:
-                x_max = unit.position[0]
+                x_max =unit.position[0]
             if unit.position[1] > y_max:
-                y_max = unit.position[1]
+                y_max =unit.position[1]
             if unit.position[0] < x_min:
-                x_min = unit.position[0]
+                x_min =unit.position[0]
             if unit.position[1] < y_min:
-                y_min = unit.position[1]
+                y_min =unit.position[1]
 
-        for obstacle in game_map.obstacles:
+        for obstacle in map.obstacles :
             if obstacle.position[0] > x_max:
-                x_max = obstacle.position[0]
+                x_max =obstacle.position[0]
             if obstacle.position[1] > y_max:
-                y_max = obstacle.position[1]
+                y_max =obstacle.position[1]
             if obstacle.position[0] < x_min:
-                x_min = obstacle.position[0]
+                x_min =obstacle.position[0]
             if obstacle.position[1] < y_min:
-                y_min = obstacle.position[1]
+                y_min =obstacle.position[1]
 
         return (x_max,x_min,y_max,y_min)

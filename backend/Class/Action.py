3c9f5200777204689a6de2489e9f82fedb600eba
Théophile BENZARIA <theophile.benzaria@gmail.com>
2026-01-25 15:35:09 +0100

    implementation heal et conversion dans test target

--- backend/Class/Action.py
+++ backend/Class/Action.py
@@ -4,10 +4,10 @@ from typing import Tuple, Union
 
 class Action:
 
-    def __init__(self, unit, king, target):
+    def __init__(self, unit, kind, target):
 
         self.unit: object   =unit              # unité qui agit
-        self.kind: str    =king                # "attack" | "move"
+        self.kind: str    =kind                # "attack" | "move"
         self.target: Union[object, Tuple[float, float]]= target
 
     def __repr__(self):

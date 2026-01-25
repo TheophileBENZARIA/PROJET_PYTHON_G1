bf2187514b916fc40d3a3e2f5d93dc7cefde9f2b
Théophile BENZARIA <theophile.benzaria@gmail.com>
2026-01-25 15:53:16 +0100

    correction hp

--- backend/Class/Units/Monk.py
+++ backend/Class/Units/Monk.py
@@ -3,7 +3,7 @@ from backend.Class.Units.Unit import Unit
 
 class Monk(Unit):
     def __init__(self, position: tuple[float]):
-        super().__init__(hp=30, attack=4, armor=0,
+        super().__init__(30, attack=4, armor=0,
                          speed=1, range_=0, reload_time=62, ligne_of_sight=11,position=position, classes=[], bonuses={})
 
     def unit_type(self) -> str:
